10c457be491b20782418851dc0762eab5859164b
Faizan Sayyed <faizansayyed36@gmail.com>
2022-09-27 19:32:23 +0530

    initial setup with basic code

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import messageHoc from './Hoc';
+
+const example = (props: any): any => <p>{props.name}, {props.message}</p>;
+
+const Message = messageHoc(example);
+
+export default Message;
